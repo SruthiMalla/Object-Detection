cc1b4a19534af25bf2fab9c46da5380b86d12549
SruthiMalla <52434141+SruthiMalla@users.noreply.github.com>
2022-10-07 16:33:29 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img = "";
 status = "";
+objects = [];
 
 function preload()
 {
@@ -28,21 +29,25 @@ function gotResult(error, results)
         console.log(error);
     }
     console.log(results);
+
+    objects = results;
 }
 
 function draw()
 {
     image(img, 0, 0, 640, 420);
 
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
-
-    fill("#FF0000");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320);
+    if(status !="")
+    {
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
